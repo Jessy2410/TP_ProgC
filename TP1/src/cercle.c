16899758d8e25834298df28d09ee0d7dcf878236
Jessy2410 <97842160+Jessy2410@users.noreply.github.com>
2025-04-16 15:06:16 +0200

    Update cercle.c

--- TP1/src/cercle.c
+++ TP1/src/cercle.c
@@ -3,15 +3,12 @@
 int main() {
     double rayon, aire, perimetre;
 
-    // Saisie du rayon
-    printf("Entrez le rayon du cercle : ");
+    putf("Entrez le rayon du cercle : ");
     scanf("%lf", &rayon);
 
-    // Calculs
     aire = M_PI * rayon * rayon;
     perimetre = 2 * M_PI * rayon;
 
-    // Affichage des résultats
     printf("Aire du cercle : %.2f\n", aire);
     printf("Périmètre du cercle : %.2f\n", perimetre);
 
