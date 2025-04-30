2b5eb96f3af476f8206debd0e44d77a09e8261aa
Jessy2410 <97842160+Jessy2410@users.noreply.github.com>
2025-04-30 14:50:07 +0200

    Update puissance.c

--- TP2/src/puissance.c
+++ TP2/src/puissance.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int a = 2;  // base
-    int b = 3;  // exposant
+    int a, b;
     int resultat = 1;
 
-    // Boucle pour calculer a^b
+    printf("Entrez la base a : ");
+    scanf("%d", &a);
+    printf("Entrez l'exposant b : ");
+    scanf("%d", &b);
+
     for (int i = 0; i < b; i++) {
         resultat *= a;
     }
-
-    // Affichage du résultat
     printf("%d élevé à la puissance %d est égal à %d\n", a, b, resultat);
 
     return 0;
