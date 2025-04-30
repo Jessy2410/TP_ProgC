3a6826fd6756defdc753611f551f4479fd3b1f21
Jessy2410 <97842160+Jessy2410@users.noreply.github.com>
2025-04-30 15:08:01 +0200

    Update bits.c

--- TP2/src/bits.c
+++ TP2/src/bits.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    unsigned int d = 0x10000008;
+    unsigned int d;
     int bit4, bit20;
+
+    printf("Entrez un entier non signé (unsigned int) : ");
+    scanf("%u", &d);
 
     bit4 = (d >> (32 - 4)) & 1;
     bit20 = (d >> (32 - 20)) & 1;
 
-    // Vérification
+    // Affichage du résultat
     if (bit4 == 1 && bit20 == 1)
         printf("1\n");
     else
